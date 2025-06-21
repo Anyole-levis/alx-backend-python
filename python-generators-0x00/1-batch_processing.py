54d946ddfe12aa5c1e13f88d03783cef2d6a543d
Anyole-levis <levisloprimo@gmail.com>
2025-06-21 13:20:18 +0300

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -3,25 +3,31 @@ def stream_users_in_batches(batch_size):
     connection = connect_to_prodev()
     cursor = connection.cursor()
 
-    cursor.execute("SELECT user_id, name, email, age FROM user_data")
+    try:
+        cursor.execute("SELECT user_id, name, email, age FROM user_data")
 
-    batch = []
-    for row in cursor:
-        batch.append(row)
-        if len(batch) == batch_size:
+        batch = []
+        for row in cursor:
+            batch.append(row)
+            if len(batch) == batch_size:
+                yield batch
+                batch = []
+
+        if batch:
             yield batch
-            batch = []
 
-    if batch:  # Yield remaining rows if any
-        yield batch
-
-    cursor.close()
-    connection.close()
+    finally:
+        cursor.close()
+        connection.close()
+        return  # graceful exit
 
 
 def batch_processing(batch_size):
     """Processes batches to yield users older than 25."""
-    for batch in stream_users_in_batches(batch_size):
-        filtered_users = [user for user in batch if float(user[3]) > 25]
-        yield filtered_users
+    try:
+        for batch in stream_users_in_batches(batch_size):
+            filtered_users = [user for user in batch if float(user[3]) > 25]
+            yield filtered_users
+    finally:
+        return  # graceful exit
 
